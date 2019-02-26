f5c7a99b5f41a2e6978266902509ab88b92eef85
Eleonor Bart <eleonorbart@zipcoders-MacBook-Pro-51.local>
2019-02-26 18:28:56 -0500

    Passed all tests

--- src/main/java/com/zipcodewilmington/looplabs/IntegerDuplicateDeleter.java
+++ src/main/java/com/zipcodewilmington/looplabs/IntegerDuplicateDeleter.java
@@ -1,8 +1,63 @@
 package com.zipcodewilmington.looplabs;
 
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
 /**
  * Created by leon on 1/29/18.
  * @ATTENTION_TO_STUDENTS You are forbidden from modifying the signature of this class.
  */
 public final class IntegerDuplicateDeleter extends DuplicateDeleter<Integer> {
+    private int count;
+    public IntegerDuplicateDeleter(Integer[] intArray) {
+        super(intArray);
+    }
+
+    @Override
+    public Integer[] removeDuplicates(int maxNumberOfDuplications) {
+        Integer[] nonDuplicates = new Integer[array.length];
+        count = 0;
+        for (int number : array) {
+            if (getNumberOfOccurances(number) < maxNumberOfDuplications) {
+                nonDuplicates[count] = number;
+                count++;
+            }
+        }
+        return getFinalArray(nonDuplicates);
+        // Not sure if we can use Arrays.copyOfRange?
+        // return Arrays.copyOfRange(nonDuplicates, 0, count);
+    }
+
+    @Override
+    public Integer[] removeDuplicatesExactly(int exactNumberOfDuplications) {
+        Integer[] nonDuplicates = new Integer[array.length];
+        count = 0;
+        for (int number : array) {
+            if (!(exactNumberOfDuplications == getNumberOfOccurances(number))) {
+                nonDuplicates[count] = number;
+                count++;
+            }
+        }
+        return getFinalArray(nonDuplicates);
+        // Not sure if we can use Arrays.copyOfRange?
+        // return Arrays.copyOfRange(nonDuplicates, 0, count);
+    }
+
+    public int getNumberOfOccurances(int testInteger) {
+        int count = 0;
+        for (int number : array) {
+            if (number == testInteger) {
+                count++;
+            }
+        }
+        return count;
+    }
+
+    public Integer[] getFinalArray(Integer[] nonDuplicates) {
+        Integer[] finalArray = new Integer[count];
+        for (int i = 0; i < count; i++) {
+            finalArray[i] = nonDuplicates[i];
+        }
+        return finalArray;
+    }
 }
--- src/main/java/com/zipcodewilmington/looplabs/StringDuplicateDeleter.java
+++ src/main/java/com/zipcodewilmington/looplabs/StringDuplicateDeleter.java
@@ -1,8 +1,63 @@
 package com.zipcodewilmington.looplabs;
 
+import java.util.Arrays;
+
 /**
  * Created by leon on 1/28/18.
  * @ATTENTION_TO_STUDENTS You are forbidden from modifying the signature of this class.
  */
 public final class StringDuplicateDeleter extends DuplicateDeleter<String> {
+
+    private int count;
+    public StringDuplicateDeleter(String[] stringArray) {
+        super(stringArray);
+    }
+
+    @Override
+    public String[] removeDuplicates(int maxNumberOfDuplications) {
+        String[] nonDuplicates = new String[array.length];
+        count = 0;
+        for (String string : array) {
+            if (getNumberOfOccurances(string) < maxNumberOfDuplications) {
+                nonDuplicates[count] = string;
+                count++;
+            }
+        }
+        return getFinalArray(nonDuplicates);
+        // Not sure if we can use Arrays.copyOfRange?
+        // return Arrays.copyOfRange(nonDuplicates, 0, count);
+    }
+
+    @Override
+    public String[] removeDuplicatesExactly(int exactNumberOfDuplications) {
+        String[] nonDuplicates = new String[array.length];
+        count = 0;
+        for (String string : array) {
+            if (!(exactNumberOfDuplications == getNumberOfOccurances(string))) {
+                nonDuplicates[count] = string;
+                count++;
+            }
+        }
+        return getFinalArray(nonDuplicates);
+        // Not sure if we can use Arrays.copyOfRange?
+        // return Arrays.copyOfRange(nonDuplicates, 0, count);
+    }
+
+    public String[] getFinalArray(String[] nonDuplicates) {
+        String[] finalArray = new String[count];
+        for (int i = 0; i < count; i++) {
+            finalArray[i] = nonDuplicates[i];
+        }
+        return finalArray;
+    }
+
+    public int getNumberOfOccurances(String testString) {
+        int count = 0;
+        for (String string : array) {
+            if (string.equals(testString)) {
+                count++;
+            }
+        }
+        return count;
+    }
 }
