345b793e15b4ec3ea47a0f247248fafacc7f5761
Eleonor Bart <eleonorbart@zipcoders-MacBook-Pro-51.local>
2019-02-28 15:29:24 -0500

    Moved all logic into the abstract class with Generics

--- src/main/java/com/zipcodewilmington/looplabs/DuplicateDeleter.java
+++ src/main/java/com/zipcodewilmington/looplabs/DuplicateDeleter.java
@@ -1,16 +1,49 @@
 package com.zipcodewilmington.looplabs;
 
+import java.util.Arrays;
+
 /**
  * Created by leon on 1/25/18.
  */
 public abstract class DuplicateDeleter<T> implements DuplicateDeleterInterface<T> {
     protected T[] array;
+    private int count;
 
     public DuplicateDeleter(T[] intArray) {
         this.array = intArray;
     }
 
-    abstract public T[] removeDuplicates(int maxNumberOfDuplications);
-    abstract public T[] removeDuplicatesExactly(int exactNumberOfDuplications);
-    //abstract public int getNumberOfOccurances(T element);
+    public T[] removeDuplicates(int maxNumberOfDuplications) {
+        T[] nonDuplicates = array.clone();
+        count = 0;
+        for (T element : array) {
+            if (getNumberOfOccurances(element) < maxNumberOfDuplications) {
+                nonDuplicates[count] = element;
+                count++;
+            }
+        }
+        return Arrays.copyOfRange(nonDuplicates, 0, count);
+    }
+
+    public T[] removeDuplicatesExactly(int exactNumberOfDuplications) {
+        T[] nonDuplicates = array.clone();
+        count = 0;
+        for (T element : array) {
+            if (!(exactNumberOfDuplications == getNumberOfOccurances(element))) {
+                nonDuplicates[count] = element;
+                count++;
+            }
+        }
+        return Arrays.copyOfRange(nonDuplicates, 0, count);
+    }
+
+    public int getNumberOfOccurances(T object) {
+        int count = 0;
+        for (T element : array) {
+            if (element == object) {
+                count++;
+            }
+        }
+        return count;
+    }
 }
--- src/main/java/com/zipcodewilmington/looplabs/IntegerDuplicateDeleter.java
+++ src/main/java/com/zipcodewilmington/looplabs/IntegerDuplicateDeleter.java
@@ -8,56 +8,17 @@ import java.util.Arrays;
  * @ATTENTION_TO_STUDENTS You are forbidden from modifying the signature of this class.
  */
 public final class IntegerDuplicateDeleter extends DuplicateDeleter<Integer> {
-    private int count;
     public IntegerDuplicateDeleter(Integer[] intArray) {
         super(intArray);
     }
 
     @Override
-    public Integer[] removeDuplicates(int maxNumberOfDuplications) {
-        Integer[] nonDuplicates = new Integer[array.length];
-        count = 0;
-        for (int number : array) {
-            if (getNumberOfOccurances(number) < maxNumberOfDuplications) {
-                nonDuplicates[count] = number;
-                count++;
-            }
-        }
-        return getFinalArray(nonDuplicates);
-        // Not sure if we can use Arrays.copyOfRange?
-        // return Arrays.copyOfRange(nonDuplicates, 0, count);
-    }
-
-    @Override
     public Integer[] removeDuplicatesExactly(int exactNumberOfDuplications) {
-        Integer[] nonDuplicates = new Integer[array.length];
-        count = 0;
-        for (int number : array) {
-            if (!(exactNumberOfDuplications == getNumberOfOccurances(number))) {
-                nonDuplicates[count] = number;
-                count++;
-            }
-        }
-        return getFinalArray(nonDuplicates);
-        // Not sure if we can use Arrays.copyOfRange?
-        // return Arrays.copyOfRange(nonDuplicates, 0, count);
+        return  super.removeDuplicatesExactly(exactNumberOfDuplications);
     }
 
-    public int getNumberOfOccurances(int testInteger) {
-        int count = 0;
-        for (int number : array) {
-            if (number == testInteger) {
-                count++;
-            }
-        }
-        return count;
+    public Integer[] removeDuplicates(int maxNumberOfDuplications) {
+        return super.removeDuplicates(maxNumberOfDuplications);
     }
 
-    public Integer[] getFinalArray(Integer[] nonDuplicates) {
-        Integer[] finalArray = new Integer[count];
-        for (int i = 0; i < count; i++) {
-            finalArray[i] = nonDuplicates[i];
-        }
-        return finalArray;
-    }
 }
